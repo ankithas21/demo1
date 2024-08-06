4aaed6b70ed327f77f3067e4737f3bc72eb9a794
ankitha.s <ankitha.s@sclera.com>
2024-08-06 16:40:03 +0530

    sonarnew2 commit

--- src/main/java/com/example/sonarDemo/SonarDemoApplication.java
+++ src/main/java/com/example/sonarDemo/SonarDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SonarDemoApplication {
 
 	public static void main(String[] args) {
-
+		System.out.println("hello");
 		SpringApplication.run(SonarDemoApplication.class, args);
 	}
 
